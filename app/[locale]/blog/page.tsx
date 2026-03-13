9a11d711220ac5d1f54a9e3155c47cac9c1602a2
Abbas Roholamin <abbasroholamin76@gmail.com>
2026-03-13 17:35:47 +0430

    feat: Add localization to blog title

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,11 +1,14 @@
+import { getTranslations } from 'next-intl/server';
 import BlogList from '@/components/blog/BlogList';
 
-export default function Page() {
+export default async function Page() {
+  const t = await getTranslations('pages');
+
   return (
     <section className="py-20">
       <div className="wrapper">
         <h1 className="text-primary font-quick-sand mb-12 text-center text-7xl font-bold">
-          Blog
+          {t('blog.title')}
         </h1>
         <BlogList />
       </div>
