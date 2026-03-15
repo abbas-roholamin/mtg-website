90aa4902398444aeeee3189c2b002b2f0f4c237e
Abbas Roholamin <abbasroholamin76@gmail.com>
2026-03-15 12:19:04 +0430

    feat: Make hero backdrop darker and link CTA button

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -2,9 +2,12 @@
 
 import { useRef, useEffect } from 'react';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 import { Button } from '../ui/button';
+import { useSetting } from '@/providers/SettingProvider';
 
 export default function Hero() {
+  const setting = useSetting();
   const videoRef = useRef<HTMLVideoElement>(null);
   const t = useTranslations('pages.home');
 
@@ -18,21 +21,25 @@ export default function Hero() {
     <div>
       <div className="relative flex h-[calc(100vh-40px)] items-center justify-center">
         <div className="relative z-30 grid content-center justify-center gap-3 sm:gap-4 xl:gap-8">
-          <h1 className="font-quick-sand text-center text-4xl font-bold text-white capitalize sm:text-6xl lg:text-7xl xl:text-8xl xl:leading-24">
-            My Travel Games
+          <h1 className="font-quick-sand text-center text-4xl font-bold text-white uppercase sm:text-6xl lg:text-7xl xl:text-8xl">
+            {setting.name}
           </h1>
 
           <div className="flex flex-col items-center justify-between gap-6 sm:gap-8">
-            <div className="font-quick-sand max-w-[340px] text-center text-lg leading-tight font-bold text-neutral-100 sm:max-w-[500px] sm:text-xl lg:max-w-[700px] lg:text-2xl xl:text-[28px]">
+            <div className="max-w-85 text-center text-lg leading-tight font-bold text-neutral-100 sm:max-w-125 sm:text-xl lg:max-w-175 lg:text-2xl xl:text-[28px]">
               {t('description')}
             </div>
-            <Button className="flex w-max items-center gap-2" size={'lg'}>
-              {t('button')}
+            <Button
+              className="flex w-max items-center gap-2 uppercase"
+              size="lg"
+              asChild
+            >
+              <Link href="/shop">{t('button')}</Link>
             </Button>
           </div>
         </div>
 
-        <div className="absolute top-0 left-0 z-20 h-full w-full bg-black/50"></div>
+        <div className="absolute top-0 left-0 z-20 h-full w-full bg-black/70"></div>
         <div className="absolute inset-0">
           <video
             ref={videoRef}
