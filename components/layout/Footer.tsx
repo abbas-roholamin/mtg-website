b820a007feffc6ed11019d59545f03ae577f18df
Abbas Roholamin <abbasroholamin76@gmail.com>
2026-03-19 11:26:15 +0430

    feat: Update footer design

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,150 +2,114 @@
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import { FacebookIcon, InstagramIcon, LinkedinIcon } from 'lucide-react';
+import Image from 'next/image';
 import Logo from '../common/Logo';
 import { MENU } from '@/constants/menu';
 import { useSetting } from '@/providers/SettingProvider';
 
 export default function Footer() {
   const n = useTranslations('navigation');
-  const f = useTranslations('layout.footer');
   const setting = useSetting();
 
   return (
-    <footer className="bg-[#48CAE4]">
+    <footer className="bg-neutral-50">
       <section className="wrapper">
         <div className="py-16">
-          <div className="mb-8 grid grid-cols-1 gap-10 sm:mb-32 sm:grid-cols-2 lg:grid-cols-4">
-            <div>
-              <Logo className="mb-8 size-24" />
-              <ul className="space-y-1.5">
-                <li>
-                  <a
-                    href={`tel:${setting.phone}`}
-                    className="font-normal text-gray-100 hover:underline"
-                  >
-                    {setting.phone}
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href={`mailto:${setting.email}`}
-                    className="font-normal text-gray-100 hover:underline"
-                  >
-                    {setting.email}
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="mb-3 font-semibold text-gray-100 sm:mb-6">
-                {f('legal')}
-              </h3>
-              <ul className="space-y-1.5">
-                <li>
-                  <Link
-                    href="/page/terms"
-                    className="hover:text-primary text-gray-100 transition-colors"
-                  >
-                    {n('terms')}
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/page/privacy-policy"
-                    className="hover:text-primary text-gray-100 transition-colors"
-                  >
-                    {n('privacy')}
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/page/imprint"
-                    className="hover:text-primary text-gray-100 transition-colors"
-                  >
-                    {n('imprint')}
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="mb-3 font-semibold text-gray-100 sm:mb-6">
-                {f('quick_links')}
-              </h3>
-              <ul className="space-y-1.5">
-                {MENU.map(item => (
-                  <li key={item.label}>
-                    <Link
-                      href={item.href}
-                      className="hover:text-primary text-gray-100 transition-colors"
-                    >
-                      {n(item.label)}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h3 className="mb-3 font-semibold text-gray-100 sm:mb-6">
-                {f('support')}
-              </h3>
-              <ul className="space-y-1.5">
-                <li>
-                  <Link
-                    href="/contact"
-                    className="hover:text-primary text-gray-100 transition-colors"
-                  >
-                    {n('contact')}
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/faqs"
-                    className="hover:text-primary text-gray-100 transition-colors"
-                  >
-                    {n('faqs')}
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
-            <ul className="flex items-center gap-4">
-              <li>
-                {' '}
-                <a
-                  href={`https:/${setting.instagram}`}
-                  target="_blank"
-                  rel="noreferrer"
+          <ul className="mb-16 flex flex-col items-center justify-center gap-6 sm:flex-row">
+            {MENU.map(item => (
+              <li key={item.label}>
+                <Link
+                  href={item.href}
+                  className="hover:text-primary font-normal text-neutral-950 transition-colors"
                 >
-                  <InstagramIcon className="text-gray-100" />
-                </a>{' '}
+                  {n(item.label)}
+                </Link>
               </li>
+            ))}
+          </ul>
+
+          <div className="mb-5 flex flex-col items-center">
+            <Logo className="mb-8 size-24" />
+            <ul className="mb-4 flex items-center gap-4">
               <li>
-                {' '}
                 <a
-                  href={`https:/${setting.facebook}`}
-                  target="_blank"
-                  rel="noreferrer"
+                  href={`tel:${setting.phone}`}
+                  className="font-normal text-neutral-600 hover:underline"
                 >
-                  <FacebookIcon className="text-gray-100" />{' '}
+                  {setting.phone}
                 </a>
               </li>
+              <li>-</li>
               <li>
-                {' '}
                 <a
-                  href={`https:/${setting.linkedin}`}
-                  target="_blank"
-                  rel="noreferrer"
+                  href={`mailto:${setting.email}`}
+                  className="font-normal text-neutral-600 hover:underline"
                 >
-                  <LinkedinIcon className="text-gray-100" />{' '}
+                  {setting.email}
                 </a>
               </li>
             </ul>
-            <p className="text-sm text-gray-100">
-              © {new Date().getFullYear()} — Copyright
-            </p>
+            <Image
+              src="/images/payment.svg"
+              alt="payments"
+              className="mx-auto"
+              width={200}
+              height={30}
+            />
           </div>
+
+          <ul className="mb-6 flex items-center justify-center gap-4">
+            <li>
+              <a
+                href={`https:/${setting.instagram}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <InstagramIcon className="text-neutral-500" />
+              </a>
+            </li>
+            <li>
+              <a
+                href={`https:/${setting.facebook}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FacebookIcon className="text-neutral-500" />{' '}
+              </a>
+            </li>
+            <li>
+              <a
+                href={`https:/${setting.linkedin}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <LinkedinIcon className="text-neutral-500" />{' '}
+              </a>
+            </li>
+          </ul>
+
+          <ul className="mb-3 flex flex-wrap items-center justify-center gap-3 md:gap-4">
+            <li>
+              <Link
+                href="/page/terms"
+                className="hover:text-primary text-sm text-neutral-500 transition-colors"
+              >
+                {n('terms')}
+              </Link>
+            </li>
+            <li className="hidden sm:list-item">-</li>
+            <li>
+              <Link
+                href="/page/privacy-policy"
+                className="hover:text-primary text-sm text-neutral-500 transition-colors"
+              >
+                {n('privacy')}
+              </Link>
+            </li>
+          </ul>
+          <p className="text-center text-sm text-neutral-500">
+            © {new Date().getFullYear()} — {setting.name}
+          </p>
         </div>
       </section>
     </footer>
