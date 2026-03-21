80165013dade1d0351d631d8872687984f6d768c
Abdul Wahed Sabury <sabury2020@gmail.com>
2026-03-21 20:51:39 +0200

    hero video updated and whereToplay section updated

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
         <div className="absolute inset-0">
           <video
             ref={videoRef}
-            src="/video.mp4"
+            src="/heroVideo.mp4"
             className="absolute top-0 left-0 h-full w-full object-cover"
             autoPlay
             muted
