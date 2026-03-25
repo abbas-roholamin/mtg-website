30abc2a3f3192bce3289af9c68ace30fbd36ceca
Abbas Roholamin <abbasroholamin76@gmail.com>
2026-03-25 14:37:18 +0430

    feat: Remove use-debounce package

--- components/product/filter/searchInput.tsx
+++ components/product/filter/searchInput.tsx
@@ -1,6 +1,5 @@
 import { SearchIcon } from 'lucide-react';
-import { useQueryState } from 'nuqs';
-import { useDebouncedCallback } from 'use-debounce';
+import { debounce, parseAsString, useQueryState } from 'nuqs';
 import {
   InputGroup,
   InputGroupAddon,
@@ -9,18 +8,20 @@ import {
 import { Field } from '@/components/ui/field';
 
 export function SearchInput() {
-  const [search, setSearch] = useQueryState('search', { defaultValue: '' });
-
-  const handleSearch = useDebouncedCallback(term => {
-    setSearch(term);
-  }, 300);
+  const [search, setSearch] = useQueryState(
+    'search',
+    parseAsString.withDefault('').withOptions({
+      shallow: false,
+      limitUrlUpdates: debounce(300),
+    })
+  );
 
   return (
     <Field className="w-sm">
       <InputGroup>
         <InputGroupInput
           value={search}
-          onChange={e => handleSearch(e.target.value)}
+          onChange={e => setSearch(e.target.value)}
           placeholder="Search..."
         />
         <InputGroupAddon align="inline-end">
