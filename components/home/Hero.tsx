4107c21fcf1978b1912c469d9f9c44bc8b99ee17
Abbas Roholamin <abbasroholamin76@gmail.com>
2026-04-18 09:22:35 +0430

    fix: Solve video path issue

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
         <div className="absolute inset-0">
           <video
             ref={videoRef}
-            src="/heroVideo.mp4"
+            src="/family-that-are-playing-game-in-air-port.mp4"
             className="absolute top-0 left-0 h-full w-full object-cover"
             autoPlay
             muted
